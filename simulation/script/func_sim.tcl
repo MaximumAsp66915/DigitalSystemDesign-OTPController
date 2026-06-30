beb906a451d609a4f3ae23ef9229c7b6a848b342
Sina Hashemi <hashemisina6@gmail.com>
2026-06-30 22:12:45 +0330

    run file created

---
+++ simulation/script/func_sim.tcl
@@ -0,0 +1,5 @@
+xrun -gui -ieee1364 -sv -disable_sem2009 -access +rwc -top tb_otp_controller \
+../rtl_sources/crc16_ccitt.v \
+../rtl_sources/otp_controller.v \
+../rtl_sources/otp_sim_rom.v \
+../rtl_sources/tb_otp_controller.v

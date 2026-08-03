219a993fa7f423b8eb6367491725058380cdcf54
Sina Hashemi <hashemisina6@gmail.com>
2026-08-03 12:19:32 +0330

    removed FUNCTIONAL_DELAY

--- simulation/script/post_syn_sim.tcl
+++ simulation/script/post_syn_sim.tcl
@@ -1,6 +1,5 @@
 xrun -gui -ieee1364 -sv -disable_sem2009 -access +rwc -top tb_otp_controller \
 +incdir+../rtl_sources \
 +define+POSTSYN_SIM \
-+define+FUNCTIONAL_DELAY \
 ../rtl_sources/otp_sim_rom.v \
 ../rtl_sources/tb_otp_controller.v
